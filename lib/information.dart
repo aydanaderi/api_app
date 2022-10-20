e12e167d1cae83765b104b157264f46c5c918519
unknown <ayda.f.naderi@gmail.com>
2022-10-20 10:49:24 +0330

    'start_information_page'

---
+++ lib/information.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Information_Page extends StatefulWidget {
+
+  @override
+  _Information_PageState createState() => _Information_PageState();
+}
+
+class _Information_PageState extends State<Information_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
