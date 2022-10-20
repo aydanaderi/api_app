98437d40754f1e429a05ede4cd38ff38ac69b608
unknown <ayda.f.naderi@gmail.com>
2022-10-20 13:08:42 +0330

    'basic_info_container'

--- lib/information.dart
+++ lib/information.dart
@@ -12,7 +12,9 @@ class _InformationPageState extends State<InformationPage> {
   Color white = Color.fromRGBO(255, 255, 255, 1);
   Color whitelight = Color.fromRGBO(234, 234, 234, 1);
   Color gray = Color.fromRGBO(144, 144, 144, 1);
-
+  Color graydot = Color.fromRGBO(183, 183, 183, 1);
+  Color grayquestion = Color.fromRGBO(112, 112, 112, 1);
+  Color grayinfo = Color.fromRGBO(80, 80, 80, 1);
   @override
   Widget build(BuildContext context) {
     
@@ -84,6 +86,135 @@ class _InformationPageState extends State<InformationPage> {
               ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Container(
+              color: white,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 10),
+                      Text(
+                        '0iajz4o474',
+                        style: TextStyle(
+                          color: grayinfo,
+                          fontFamily: 'IRANSansFaNum',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(width: 50),
+                      Text(
+                        'شناسه پرونده ',
+                        style: TextStyle(
+                          color: grayquestion,
+                          fontFamily: 'IRANSansFaNum',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                        ),
+
+                      ),
+                      Icon(
+                        Icons.circle,
+                        size: 8,
+                        color: graydot,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(width: 10),
+                      Text(
+                        'محمدعلی مراد بیگ زاده',
+                        style: TextStyle(
+                          color: grayinfo,
+                          fontFamily: 'IRANSansFaNum',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(width: 50),
+                      Text(
+                        'نام مشتری ',
+                        style: TextStyle(
+                          color: grayquestion,
+                          fontFamily: 'IRANSansFaNum',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                        ),
+
+                      ),
+                      Icon(
+                        Icons.circle,
+                        size: 8,
+                        color: graydot,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(width: 10),
+                      Text(
+                        '09127825671',
+                        style: TextStyle(
+                          color: grayinfo,
+                          fontFamily: 'IRANSansFaNum',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(width: 50),
+                      Text(
+                        'تلفن همراه',
+                        style: TextStyle(
+                          color: grayquestion,
+                          fontFamily: 'IRANSansFaNum',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                        ),
+
+                      ),
+                      Icon(
+                        Icons.circle,
+                        size: 8,
+                        color: graydot,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'شهرک غرب، فاز 4، زرافشان، خیابان شجریان، پلاک 13، واحد 8',
+                        style: TextStyle(
+                          color: grayinfo,
+                          fontFamily: 'IRANSansFaNum',
+                          fontSize: 8,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Text(
+                        'محل بازدید ',
+                        style: TextStyle(
+                          color: grayquestion,
+                          fontFamily: 'IRANSansFaNum',
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                        ),
+
+                      ),
+                      Icon(
+                        Icons.circle,
+                        size: 8,
+                        color: graydot,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
